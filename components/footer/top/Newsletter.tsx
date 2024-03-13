1676c4d32af87add31a086f6eeb48ebd24c09a8c
HugoLc <hugolindoso.c@gmail.com>
2024-03-13 14:32:46 -0300

    feat(footer): cria newsletter

--- components/footer/top/Newsletter.tsx
+++ components/footer/top/Newsletter.tsx
@@ -3,16 +3,16 @@ import React from "react";
 const Newsletter = () => {
   return (
     <div className=" border-t border-light-gray border-b">
-      <div className="flex justify-center items-center px-6 md:px-32 py-8 max-w-screen-xl mx-auto">
-        <p className="text-gray text-lg font-bold max-w-[307px] w-full pr-4 md:pr-0">
+      <div className="flex justify-center flex-col sm:flex-row items-center px-6 md:px-32 py-8 max-w-screen-xl mx-auto">
+        <p className="text-gray text-2xl text-center mb-4 sm:mb-0 font-bold max-w-[307px] w-full pr-4 md:pr-0">
           Receba Nossa Newsletter
         </p>
 
-        <form className="flex w-full">
+        <form className="flex w-full justify-between">
           <input
             type="text"
             placeholder="Digite seu e-mail"
-            className="w-full h-10 border border-gray rounded-md px-6"
+            className="w-2/3 sm:w-full h-10 border border-gray rounded-md px-6"
           />
 
           <button className="bg-[#FAA500] ml-[-8px] h-10 max-w-[131px] w-full right-0 md:static rounded-md text-white font-bold text-lg">
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import TopBar from "./top/TopBar";
 import ToggleHeader from "./ToggleHeader";
-import { headers } from "next/headers";
 //TODO: animações
 const Header = () => {
-  const headerList = headers();
-  console.log("assas", headerList.get("user-agent"));
-
   return (
     <div>
       <TopBar />
