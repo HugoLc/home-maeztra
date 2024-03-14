fe3606cd943d29bf043122afff9d26c5886fda3a
HugoLc <hugolindoso.c@gmail.com>
2024-03-13 22:07:51 -0300

    feat(tips): inicio tips

--- components/main/tipbar/TipBar.tsx
+++ components/main/tipbar/TipBar.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { dataTipbar } from "./mock";
+import "./styles.css";
 
 const TipBar = () => {
   return (
-    <div>TipBar</div>
-  )
-}
+    <section>
+      <p className="text-base font-bold text-gray-700 py-6 text-center">
+        Título
+      </p>
+      <div className="flex items-center justify-left w-[90vw] m-auto  scrollbar-hidden gap-4">
+        {dataTipbar.map((tip) => (
+          <div className="">
+            <div className=" max-w-screen-xl w-full mx-auto">
+              <div className=" flex items-center justify-center">
+                <div className="min-w-[307px] flex items-center bg-gray-200 rounded-md flex-none h-16 px-7 max-w-xs">
+                  <img src={tip.img} alt="Banner principal" />
+                  <section className="container-advantagens-slider-item-texts ml-5">
+                    <p className="text-sm font-bold text-gray-700">
+                      {tip.title}
+                    </p>
+                    <span className="text-xs font-medium text-black">
+                      {tip.subTitle}
+                    </span>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default TipBar
+export default TipBar;
---
+++ components/main/tipbar/mock.ts
@@ -0,0 +1,32 @@
+export const dataTipbar = [
+  {
+    id: Math.random(),
+    title: "Produtos importados",
+    subTitle: "Produto de Alta Qualidade",
+    img: "/icons/world-icon.svg",
+  },
+  {
+    id: Math.random(),
+    title: "Estoque no Brazil",
+    subTitle: "Produtos mais perto de você!",
+    img: "/icons/house-icon.svg",
+  },
+  {
+    id: Math.random(),
+    title: "Trocas Garantidas",
+    subTitle: "Trocas em até 48 horas, vejas as regras",
+    img: "/icons/exchanges-icon.svg",
+  },
+  {
+    id: Math.random(),
+    title: "Ganhe 5% off",
+    subTitle: "Pagando à vista no Cartão",
+    img: "/icons/discount.svg",
+  },
+  {
+    id: Math.random(),
+    title: "Frete Grátis",
+    subTitle: "Em compras acima de R$ 499,00",
+    img: "/icons/truck-icon.svg",
+  },
+];
